eb3af298233a99308b323960a57061c6dbb40408
bmouseProductions <designers@bmouseproductions.com>
2024-04-17 16:42:21 -0300

    first commit

--- src/pages/Home/Sections/SectionTwo/SectionTwo.tsx
+++ src/pages/Home/Sections/SectionTwo/SectionTwo.tsx
@@ -1,9 +1,7 @@
-import Button from "../../../../components/Button/Button";
-
 
 export default function SectionTwo() {
     return (
-        <section className=" container mx-auto px-5 md:px-10 xl:px-16 flex flex-col-reverse lg:flex-row gap-5">
+        <section className=" container mx-auto px-5 md:px-10 xl:px-16 flex flex-col gap-5">
             <div className='w-full grid grid-cols-1 md:grid-cols-2 justify-items-center gap-10 text-white'>
                 <div className='px-5 lg:px-10 py-10 lg:py-16 max-w-[500px] flex flex-col justify-center items-center gap-5 bg-[#0062ff] rounded-[30px]  '>
                     <h2 className='titleFont text-3xl text-center text-[#fff800] uppercase '>
@@ -18,7 +16,7 @@ export default function SectionTwo() {
 
                 <div className='px-5 lg:px-10  py-10 lg:py-16 max-w-[500px] flex flex-col justify-center items-center gap-5 bg-[#0062ff] rounded-[30px]  '>
                     <h2 className='titleFont text-3xl text-center text-[#fff800] uppercase '>
-                    Atrair novos bons funcionários
+                        Atrair novos bons funcionários
                     </h2>
                     <p>
                         Oferecendo um bom clube de descontos, você
@@ -29,7 +27,7 @@ export default function SectionTwo() {
 
                 <div className='px-5 lg:px-10  py-10 lg:py-16 max-w-[500px] flex flex-col justify-center items-center gap-5 bg-[#0062ff] rounded-[30px]  '>
                     <h2 className='titleFont text-3xl text-center text-[#fff800] uppercase '>
-                    Bem-estar e preocupação com o funcionário
+                        Bem-estar e preocupação com o funcionário
                     </h2>
                     <p>
                         Mostra que a empresa se preocupa com a
@@ -48,10 +46,16 @@ export default function SectionTwo() {
                         colaboradores.
                     </p>
                 </div>
+            </div>
 
-                <div>
-                    <Button estilo="amarelo"/>
-                </div>
+            <div className="mx-auto mt-5 md:mt-7">
+                <a 
+                    href="https://transtriangulo.com.br/contato/"
+                    target="_blank"
+                    className="w-[250px] py-2 px-7 text-xl md:text-3xl uppercase text-center titleFont rounded-[35px] bg-[#fff800] text-black"
+                >
+                    CONTRATE AGORA
+                </a>
             </div>
         </section>
     )
